2013e62922f57b8de957fff55336efecf1840faa
daedalussolutions <dylancassell000@hotmail.com>
2023-11-20 14:11:09 -0500

    Initial commit.

---
+++ src/components/ComboGenerator.js
@@ -0,0 +1,20 @@
+import React from 'react';
+const ComboGenerator = () =>{
+    return (
+        <div className='combo-generator-container'>
+            <h1>Boxing Combo Generator</h1>
+            <div className='combo-type-selector-container'>
+                
+
+
+
+
+            </div>
+
+
+
+
+        </div>
+    )
+}
+export default ComboGenerator;
